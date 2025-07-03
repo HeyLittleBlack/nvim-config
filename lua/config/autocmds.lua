4bfac712c731dac2ef129481db8b901abf9b4648
liuxuhao <lxhzzzzz@126.com>
2025-07-03 18:29:52 +0800

    update: config autoformat file

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -19,3 +19,15 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.spell = false
   end,
 })
+--
+-- Autoformat setting
+local set_autoformat = function(pattern, bool_val)
+  vim.api.nvim_create_autocmd({ "FileType" }, {
+    pattern = pattern,
+    callback = function()
+      vim.b.autoformat = bool_val
+    end,
+  })
+end
+
+set_autoformat({ "python" }, false)
