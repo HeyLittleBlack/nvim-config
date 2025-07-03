7006ce8fd34afdf1d11f8d704ddf29bcbfd8f8ac
liuxuhao <lxhzzzzz@126.com>
2025-07-03 18:11:42 +0800

    update: add pyright

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,11 +1,25 @@
 return {
-  -- lsp keymaps
   {
     "neovim/nvim-lspconfig",
-    --"LazyVim/LazyVim",
-    init = function()
+    opts = function()
       local keys = require("lazyvim.plugins.lsp.keymaps").get()
       keys[#keys + 1] = { "K", false }
     end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        pyright = {
+          settings = {
+            python = {
+              analysis = {
+                typeCheckingMode = "off",
+              },
+            },
+          },
+        },
+      },
+    },
   },
 }
