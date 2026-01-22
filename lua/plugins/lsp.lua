c11e712014a13fa310985355638b8f57311d2aaa
liuxuhao <lxhzzzzz@126.com>
2026-01-22 15:04:04 +0800

    update: .stop_lsp to disbale clangd

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -31,6 +31,21 @@ return {
             "clangd",
             "--fallback-style=webkit",
           },
+          on_attach = function(client, bufnr)
+            local util = require("lspconfig.util")
+            local fname = vim.api.nvim_buf_get_name(bufnr)
+
+            -- 再次检查标记文件
+            if util.root_pattern(".stop_lsp", ".no-lsp")(fname) then
+              -- 立即清空当前 buffer 的所有诊断信息（消灭红线）
+              vim.diagnostic.reset(nil, bufnr)
+              -- 立即断开这个 LSP 客户端的连接
+              vim.schedule(function()
+                client.stop()
+              end)
+              return false
+            end
+          end,
         },
       },
     },
